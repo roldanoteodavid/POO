5d1926cf4f94fde94cb57b9ed45d561dbe1697dc
Pablopume <pablo.serrano32@educa.madrid.org>
2022-12-19 14:25:00 +0100

    Programación 19/12

--- POO2/src/main/java/org/example/Practica5b.java
+++ POO2/src/main/java/org/example/Practica5b.java
@@ -5,10 +5,10 @@ public class Practica5b {
         Asignatura programacion = new Asignatura(1);
         Asignatura entornos = new Asignatura(2);
         Asignatura marcas = new Asignatura(3);
-        Alumno juan = new Alumno(programacion, entornos, marcas);
-        Profesor antonio = new Profesor();
-        antonio.ponerNotas(juan);
+        Alumno alumno = new Alumno(programacion, entornos, marcas);
+        Profesor profesor = new Profesor();
+        profesor.ponerNotas(alumno);
         System.out.println("La nota de programación es "+ programacion.getNota()+", la nota de entornos es "+entornos.getNota()+" y la nota de lenguaje de marcas es "+marcas.getNota());
-        System.out.println("La media del alumno es "+antonio.calcularMedia(juan));
+        System.out.println("La media del alumno es "+profesor.calcularMedia(alumno));
     }
 }
